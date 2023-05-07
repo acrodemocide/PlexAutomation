9c4054dc91cacb353f0a0f6846c1f8d74c60e4c9
Daniel Howard <acrodemocide@hotmail.com>
2023-05-07 11:27:29 -0600

    Generated test folder full of mkv files to ensure that we're able to select the largest one

--- py_movies.py
+++ py_movies.py
@@ -1,7 +1,8 @@
 from os import listdir, stat
 from os.path import isfile, join
 
-path = "."
+# TODO: dhoward -- get this via command-line args
+path = "./test_movies"
 only_mkv_files = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith('.mkv')]
 
 for file in only_mkv_files:
--- py_movies_test.py
+++ py_movies_test.py
@@ -1,6 +1,26 @@
 import os
+from random import randrange
 
 new_path = "./test_movies"
+number_of_files = 10
+largest_file_size = 1000
+
+def generate_test_file(file_name, number_of_bytes):
+    fp = open(file_name, "w")
+    for j in range(number_of_bytes):
+        fp.write("a")
+    fp.close()
+
 
 if not os.path.exists(new_path):
     os.makedirs(new_path)
+
+for i in range(number_of_files):
+    file_size = 0
+    # Create largest file here
+    if i == 0:
+        file_size = largest_file_size
+    else:
+        file_size = randrange(1, (largest_file_size - 1), 1)
+    
+    generate_test_file(f"{new_path}/file{i}.mkv", file_size)
