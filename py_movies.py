2e0c46b29cc6d7537bbc3b7bdd6ec9629f3c52c3
Daniel Howard <acrodemocide@hotmail.com>
2023-05-07 11:11:59 -0600

    Updated var naming to file Python convention. Created test to add new test_movies folder to start testing mkv files

--- py_movies.py
+++ py_movies.py
@@ -2,8 +2,8 @@ from os import listdir, stat
 from os.path import isfile, join
 
 path = "."
-onlyMkvFiles = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith('.mkv')]
+only_mkv_files = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith('.mkv')]
 
-for file in onlyMkvFiles:
+for file in only_mkv_files:
     size = stat(file).st_size
     print(f"File: {file} -- size: {size}")
--- py_movies_test.py
+++ py_movies_test.py
@@ -1,1 +1,6 @@
-print("This is the test")
+import os
+
+new_path = "./test_movies"
+
+if not os.path.exists(new_path):
+    os.makedirs(new_path)
