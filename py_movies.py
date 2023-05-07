bb48c7f145835f011a03dc5711fbf5f10e6e205e
Daniel Howard <acrodemocide@hotmail.com>
2023-05-07 11:09:04 -0600

    Updated string styles to use double-quotes

--- py_movies.py
+++ py_movies.py
@@ -1,9 +1,9 @@
 from os import listdir, stat
 from os.path import isfile, join
 
-path = '.'
+path = "."
 onlyMkvFiles = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith('.mkv')]
 
 for file in onlyMkvFiles:
     size = stat(file).st_size
-    print(f'File: {file} -- size: {size}')
+    print(f"File: {file} -- size: {size}")
