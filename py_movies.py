47aca02826ce01f609fa4bd75c7313f70c870c32
Daniel Howard <acrodemocide@hotmail.com>
2023-05-07 11:06:01 -0600

    Added automated test file for movies

--- py_movies.py
+++ py_movies.py
@@ -1,6 +1,8 @@
 from os import listdir, stat
 from os.path import isfile, join
-onlyMkvFiles = [f for f in listdir('.') if isfile(join('.', f)) and f.endswith('.mkv')]
+
+path = '.'
+onlyMkvFiles = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith('.mkv')]
 
 for file in onlyMkvFiles:
     size = stat(file).st_size
