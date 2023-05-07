0102ba3af37b6470b19b4260acf8f42b20f3b5de
Daniel Howard <acrodemocide@hotmail.com>
2023-05-07 11:36:15 -0600

    Updated script to get largest file from list of files

--- py_movies.py
+++ py_movies.py
@@ -5,6 +5,15 @@ from os.path import isfile, join
 path = "./test_movies"
 only_mkv_files = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith('.mkv')]
 
+largest_file_size = 0
+largest_file = ""
 for file in only_mkv_files:
-    size = stat(file).st_size
+    # print(f"File: {file}")
+    full_file_path = f"{path}/{file}"
+    size = stat(full_file_path).st_size
+    if size > largest_file_size:
+        largest_file_size = size
+        largest_file = file
     print(f"File: {file} -- size: {size}")
+
+print(f"largest_file: {largest_file} -- largest_file_size: {largest_file_size}")
