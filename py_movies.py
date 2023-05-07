f546a25ec4e20501bbc2ca2867d1800488cb20e2
Daniel Howard <acrodemocide@hotmail.com>
2023-05-07 11:42:52 -0600

    Added logic to remove files to be deleted

--- py_movies.py
+++ py_movies.py
@@ -1,4 +1,4 @@
-from os import listdir, stat
+from os import listdir, stat, remove
 from os.path import isfile, join
 
 # TODO: dhoward -- get this via command-line args
@@ -17,4 +17,10 @@ for file in only_mkv_files:
 
 print(f"largest_file: {largest_file} -- largest_file_size: {largest_file_size}")
 
-# [f for f in only_mkv_files if ]
+files_to_delete = [f for f in only_mkv_files if f != largest_file]
+print("Files to delete:")
+for file in files_to_delete:
+    full_file_path = f"{path}/{file}"
+    size = stat(full_file_path).st_size
+    print(f"File: {file} -- size: {size}")
+    remove(full_file_path)
