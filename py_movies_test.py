71928b3973007f7bba7cf1d514e2dfccda237b16
Daniel Howard <acrodemocide@hotmail.com>
2023-05-14 09:55:45 -0600

    Updated py_movies_test.py to use generic file creation in utils

--- py_movies_test.py
+++ py_movies_test.py
@@ -2,18 +2,21 @@ import math
 import os
 from random import randrange
 from os.path import join, isdir
-from file_and_directory_util import generate_directories
+from file_and_directory_util import generate_directories, generate_file
 
 base_path = "./movies"
 number_of_movies = 10
 number_of_files = 10
 largest_file_size = 1000
 
-def generate_test_file(file_name, number_of_bytes):
-    fp = open(file_name, "w")
-    for j in range(number_of_bytes):
-        fp.write("a")
-    fp.close()
+# def generate_test_file(file_name, number_of_bytes):
+#     fp = open(file_name, "w")
+#     for j in range(number_of_bytes):
+#         fp.write("a")
+#     fp.close()
+
+if not os.path.exists(base_path):
+    os.makedirs(base_path)
 
 # generate test movie folders
 base_dir_title = "test movie"
@@ -31,4 +34,4 @@ for movie_folder in generated_movie_folders:
         else:
             file_size = randrange(1, (largest_file_size - 1), 1)
         
-        generate_test_file(f"{base_path}/{movie_folder}/file{i}.mkv", file_size)
+        generate_file(f"{base_path}/{movie_folder}/file{i}.mkv", file_size)
