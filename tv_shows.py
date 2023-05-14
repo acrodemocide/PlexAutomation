e7da0f7cf8249b43f44a129c429cd8679825e97b
Daniel Howard <acrodemocide@hotmail.com>
2023-05-14 16:51:39 -0600

    Added basic functionality for listing directories for tv shows

--- tv_shows.py
+++ tv_shows.py
@@ -1,1 +1,15 @@
-print("Hello")
+import os
+from os.path import join, isdir
+
+base_path = "tv_shows"
+
+def get_child_directories(path):
+    children = [f for f in os.listdir(path) if isdir(join(path, f))]
+    return children
+
+tv_shows = get_child_directories(base_path)
+print(tv_shows)
+
+tv_show = join(base_path, tv_shows[0])
+seasons = get_child_directories(tv_show)
+print(seasons)
