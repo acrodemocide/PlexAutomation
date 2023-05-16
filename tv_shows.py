624ae1abf9902077e73e02bda7d988a3a70d51a5
Daniel Howard <acrodemocide@hotmail.com>
2023-05-15 20:04:08 -0600

    Updated tv file script to rename all episodes contained in TV Series properly

--- tv_shows.py
+++ tv_shows.py
@@ -1,4 +1,5 @@
 import os
+from os import rmdir, rename
 from os.path import join, isdir
 
 base_path = "tv_shows"
@@ -13,16 +14,32 @@ def get_child_files(path):
 
 tv_shows = get_child_directories(base_path)
 for tv_show in tv_shows:
-    print(tv_show)
     tv_show_path = join(base_path, tv_show)
     seasons = get_child_directories(tv_show_path)
     for season in seasons:
-        print(season)
+        season_number = int(season.split()[1])
         season_path = join(tv_show_path, season)
+        if (season_number < 10):
+            file_name_prefix = f"{tv_show} - s0{season_number}"
+        else:
+            file_name_prefix = f"{tv_show} - s{season_number}"
+        current_episode_number = 1
         discs = get_child_directories(season_path)
         for disc in discs:
-            print(disc)
             disc_path = join(season_path, disc)
             files = get_child_files(disc_path)
+            # Remove files that are not episodes
             files.sort()
-            print(files)
+            for file in files:
+                if (current_episode_number < 10):
+                    new_file_name = f"{file_name_prefix}e0{current_episode_number}.mkv"
+                else:
+                    new_file_name = f"{file_name_prefix}e{current_episode_number}.mkv"
+                current_episode_number += 1
+                cur_file_path = join(disc_path, file)
+                new_file_path = join(season_path, new_file_name)
+                rename(cur_file_path, new_file_path)
+            rmdir(disc_path)
+
+
+            
