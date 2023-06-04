7758cf780e34625615b2108534f0b5697ae176a8
Daniel Howard <acrodemocide@hotmail.com>
2023-06-04 10:54:37 -0600

    Updated test to use dictionary to verify end result of test and moved unneeded method back into caller

--- py_tv_shows_test.py
+++ py_tv_shows_test.py
@@ -166,7 +166,7 @@ def generate_tv_shows(base_path, tv_shows_dictionary):
                 generate_file(episode_path, episode_size)
 
 
-def verify_tv_shows(base_path, tv_shows_dictionary):
+def verify_unprocessed_tv_shows(base_path, tv_shows_dictionary):
     if not os.path.exists(base_path):
         print(f"Failed! Base Path {base_path} does not exist")
 
@@ -200,18 +200,41 @@ def verify_tv_shows(base_path, tv_shows_dictionary):
                 if actual_episodes[k] != expected_episodes[k]["name"]:
                     print(f"Failed! Acutal episode name {actual_episodes[k]} not equal to expected episode name {expected_episodes[k]['name']}")
 
+def verify_processed_tv_shows(base_path, tv_shows_dictionary):
+    if not os.path.exists(base_path):
+        print(f"Failed! Base Path {base_path} does not exist")
 
-def generate_generic_tv_shows(
-    base_path,
-    number_of_tv_shows,
-    number_of_seasons,
-    number_of_discs_per_season,
-    number_of_episodes_per_disc):
+    tv_show_path = join(base_path, tv_shows_dictionary["name"])
+    actual_seasons = os.listdir(tv_show_path)
+    expected_seasons = tv_shows_dictionary["seasons"]
+    if len(actual_seasons) != len(expected_seasons):
+        print(f"Failed! Actual number of seasons {len(actual_seasons)} != expected {len(expected_seasons)}")
+
+    for i in range(len(actual_seasons)):
+        season_path = join(tv_show_path, actual_seasons[i])
+        if actual_seasons[i] != expected_seasons[i]["name"]:
+            print(f"Failed! Actual season name {actual_seasons[i]} not equal to expected season name {expected_seasons[i]['name']}")
+
+        actual_episodes = os.listdir(season_path)
+        expected_episodes = expected_seasons[i]["episodes"]
+        if len(actual_episodes) != len(expected_episodes):
+            print(f"Failed! Actual number of episodes {len(actual_episodes)} != expected {len(expected_episodes)}")
+            
+        for j in range(len(actual_episodes)):
+            if actual_episodes[j] != expected_episodes[j]:
+                print(f"Failed! Actual episode name {actual_episodes[j]} not equal to expected episode name {expected_episodes[j]}")
+        
+def generic_tv_shows_test():
+    base_path = "tv_shows"
+    number_of_tv_shows = 10
+    number_of_seasons = 5
+    number_of_discs_per_season = 4
+    number_of_episodes_per_disc = 5
 
     if not os.path.exists(base_path):
         os.makedirs(base_path)
 
-    tv_shows = []
+    generated_tv_shows = []
     for i in range(number_of_tv_shows):
         tv_show = {
             "name": f"test ({1990 + i})",
@@ -236,52 +259,43 @@ def generate_generic_tv_shows(
                     disc["episodes"].append(episode)
                 season["discs"].append(disc)
             tv_show["seasons"].append(season)
-        tv_shows.append(tv_show)
+        generated_tv_shows.append(tv_show)
 
 
-    for tv_show in tv_shows:       
+    for tv_show in generated_tv_shows:       
         generate_tv_shows(base_path, tv_show)
-    
-    return tv_shows
-        
-def generic_tv_shows_test():
-    base_path = "tv_shows"
-    number_of_tv_shows = 10
-    number_of_seasons = 5
-    number_of_discs_per_show = 4
-    number_of_episodes_per_disc = 5
-
-    generated_tv_shows = generate_generic_tv_shows(
-        base_path, 
-        number_of_tv_shows,
-        number_of_seasons,
-        number_of_discs_per_show, 
-        number_of_episodes_per_disc)
 
     # Verify tv shows laid out as expected
     if not os.path.exists(base_path):
         print(f"Failed! Base Path {base_path} does not exist")
     for tv_show in generated_tv_shows:
-        verify_tv_shows(base_path, tv_show)
+        verify_unprocessed_tv_shows(base_path, tv_show)
 
     # Run the test
     os.system("py_tv_shows.py tv_shows")
 
     # Verify the results
-    tv_shows = os.listdir(base_path)
-    total_episodes_in_season = number_of_discs_per_show * number_of_episodes_per_disc
-    for tv_show in tv_shows:
-        tv_show_path = join(base_path, tv_show)
-        seasons = os.listdir(tv_show_path)
-        if len(seasons) != number_of_seasons:
-            print(f"Failed! Expected {number_of_seasons} seasons, but found {len(seasons)}")
-        for season in seasons:
-            season_path = join(tv_show_path, season)
-            directories = [f for f in os.listdir(season_path) if isdir(join(season_path, f))]
-            if len(directories) != 0:
-                print(f"Failed! Expected no directories but found {len(directories)} in season {season}")
-            files = [f for f in os.listdir(season_path) if not isdir(join(season_path, f))]
-            if len(files) != total_episodes_in_season:
-                print(f"Failed! Expected {total_episodes_in_season} episodes, but found only {len(files)}")
+    processed_tv_shows = []
+    for tv_show in generated_tv_shows:
+        processed_tv_show = {
+            "name": tv_show["name"],
+            "seasons": []
+        }
+
+        for i in range(number_of_seasons):
+            season = {
+                "name": f"Season {i + 1}",
+                "episodes": []
+            }
+            for j in range(number_of_discs_per_season * number_of_episodes_per_disc):
+                season_number = f"0{i+1}" if i + 1 < 10 else f"{i+1}"
+                episode_number = f"0{j+1}" if j + 1 < 10 else f"{j+1}"
+                season["episodes"].append(f"{tv_show['name']} - s{season_number}e{episode_number}.mkv")
+            processed_tv_show["seasons"].append(season)
+        processed_tv_shows.append(processed_tv_show)
+    
+    for processed_tv_show in processed_tv_shows:
+        verify_processed_tv_shows(base_path, processed_tv_show)
+
 
 generic_tv_shows_test()
