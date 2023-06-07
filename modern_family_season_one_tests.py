52ea35c3f13eb350e86ee8978ee2911e6ab33bd2
Daniel Howard <acrodemocide@hotmail.com>
2023-06-06 21:59:31 -0600

    Created utility file for tests. Updated test files

---
+++ modern_family_season_one_tests.py
@@ -0,0 +1,248 @@
+import os
+from test_utilities import generate_tv_shows, verify_unprocessed_tv_shows
+
+def season_01_test():
+    base_path = "tv_shows"
+    tv_show = {
+        "name": "Modern Family (2009)",
+        "seasons": [
+            {
+                "name": "Season 1",
+                "discs": [
+                    {
+                        "name": "MODERNFAMILY_S1_D1",
+                        "episodes": [
+                            {
+                                "name": "A1_t08.mkv",
+                                "size": 91
+                            },
+                            {
+                                "name": "B1_t00.mkv",
+                                "size": 6200
+                            },
+                            {
+                                "name": "C1_t01.mkv",
+                                "size": 1100
+                            },
+                            {
+                                "name": "C2_t02.mkv",
+                                "size": 1000
+                            },
+                            {
+                                "name": "C3_t03.mkv",
+                                "size": 1000
+                            },
+                            {
+                                "name": "C4_t04.mkv",
+                                "size": 996
+                            },
+                            {
+                                "name": "C5_t05.mkv",
+                                "size": 1100
+                            },
+                            {
+                                "name": "C6_t06.mkv",
+                                "size": 1000
+                            },
+                            {
+                                "name": "C7_t07.mkv",
+                                "size": 6200
+                            },
+                            {
+                                "name": "D1_t12.mkv",
+                                "size": 312
+                            },
+                            {
+                                "name": "D2_t10.mkv",
+                                "size": 185
+                            },
+                            {
+                                "name": "E1_t09.mkv",
+                                "size": 324
+                            },
+                            {
+                                "name": "E4_t11.mkv",
+                                "size": 104
+                            }
+                        ]
+                    },
+                    {
+                        "name": "MODERNFAMILY_S1_D2",
+                        "episodes": [
+                            {
+                                "name": "B1_t00.mkv",
+                                "size": 6000
+                            },
+                            {
+                                "name": "C1_t01.mkv",
+                                "size": 924
+                            },
+                            {
+                                "name": "C2_t02.mkv",
+                                "size": 923
+                            },
+                            {
+                                "name": "C3_t03.mkv",
+                                "size": 1000
+                            },
+                            {
+                                "name": "C4_t04.mkv",
+                                "size": 1100
+                            },
+                            {
+                                "name": "C5_t05.mkv",
+                                "size": 1100
+                            },
+                            {
+                                "name": "C6_t06.mkv",
+                                "size": 950
+                            },
+                            {
+                                "name": "C7_t07.mkv",
+                                "size": 6000
+                            },
+                            {
+                                "name": "D1_t08.mkv",
+                                "size": 882
+                            },
+                            {
+                                "name": "D2_t09.mkv",
+                                "size": 92
+                            },
+                            {
+                                "name": "D3_t10.mkv",
+                                "size": 146
+                            },
+                            {
+                                "name": "D4_t11.mkv",
+                                "size": 244
+                            },
+                            {
+                                "name": "D5_t12.mkv",
+                                "size": 310
+                            },
+                            {
+                                "name": "D6_t13.mkv",
+                                "size": 90
+                            }
+                        ]
+                    },
+                    {
+                        "name": "MODERNFAMILY_S1_D3",
+                        "episodes": [
+                            {
+                                "name": "B1_t00.mkv",
+                                "size": 6100
+                            },
+                            {
+                                "name": "C1_t01.mkv",
+                                "size": 1000
+                            },
+                            {
+                                "name": "C2_t02.mkv",
+                                "size": 1000
+                            },
+                            {
+                                "name": "C3_t03.mkv",
+                                "size": 1000
+                            },
+                            {
+                                "name": "C4_t04.mkv",
+                                "size": 945
+                            },
+                            {
+                                "name": "C5_t05.mkv",
+                                "size": 1000
+                            },
+                            {
+                                "name": "C6_t06.mkv",
+                                "size": 1000
+                            },
+                            {
+                                "name": "C7_t07.mkv",
+                                "size": 6100
+                            },
+
+                            {
+                                "name": "E1_t08.mkv",
+                                "size": 242
+                            }
+                        ]
+                    },
+                    {
+                        "name": "MODERNFAMILY_S1_D4",
+                        "episodes": [
+                            {
+                                "name": "B1_t00.mkv",
+                                "size": 5700
+                            },
+                            {
+                                "name": "C1_t01.mkv",
+                                "size": 963
+                            },
+                            {
+                                "name": "C2_t02.mkv",
+                                "size": 940
+                            },
+                            {
+                                "name": "C3_t03.mkv",
+                                "size": 969
+                            },
+                            {
+                                "name": "C4_t04.mkv",
+                                "size": 920
+                            },
+                            {
+                                "name": "C5_t05.mkv",
+                                "size": 951
+                            },
+                            {
+                                "name": "C6_t06.mkv",
+                                "size": 948
+                            },
+                            {
+                                "name": "C7_t07.mkv",
+                                "size": 5700
+                            },
+                            {
+                                "name": "D1_t08.mkv",
+                                "size": 246
+                            },
+                            {
+                                "name": "D2_t09.mkv",
+                                "size": 212
+                            },
+                            {
+                                "name": "D3_t10.mkv",
+                                "size": 390
+                            },
+                            {
+                                "name": "E1_t11.mkv",
+                                "size": 485
+                            },
+                            {
+                                "name": "E2_t14.mkv",
+                                "size": 156
+                            },
+                            {
+                                "name": "E3_t12.mkv",
+                                "size": 339
+                            },
+                            {
+                                "name": "E4_t13.mkv",
+                                "size": 197
+                            }
+                        ]
+                    },
+                ]
+            }
+        ]
+    }
+    generate_tv_shows(base_path, tv_show)
+
+    # Verify tv shows laid out as expected
+    if not os.path.exists(base_path):
+        print(f"Failed! Base Path {base_path} does not exist")
+    verify_unprocessed_tv_shows(base_path, tv_show)
+
+season_01_test()
--- py_tv_shows_test.py
+++ py_tv_shows_test.py
@@ -1,6 +1,7 @@
 import os
-from file_and_directory_util import generate_file
-from os.path import join
+# from file_and_directory_util import generate_file
+# from os.path import join
+from test_utilities import generate_tv_shows, verify_unprocessed_tv_shows, verify_processed_tv_shows
 
 # Option B -- Use Dictionaries to define your folder structure, then traverse over the
 #   dictionary to either generate that folder structure or verify the existing structure
@@ -142,91 +143,7 @@ unprocessed_tv_shows = {
         }
     ]
 }
-
-
-def generate_tv_shows(base_path, tv_shows_dictionary):
-    tv_show_path = join(base_path, tv_shows_dictionary["name"])
-    os.makedirs(tv_show_path)
-
-    for season in tv_shows_dictionary["seasons"]:
-        season_name = season["name"]
-        season_path = join(tv_show_path, season_name)
-        os.makedirs(season_path)
-
-        discs = season["discs"]
-        for disc in discs:
-            disc_name = disc["name"]
-            disc_path = join(season_path, disc_name)
-            os.makedirs(disc_path)
-
-            episodes = disc["episodes"]
-            for episode in episodes:
-                episode_name = episode["name"]
-                episode_path = join(disc_path, episode_name)
-                episode_size = episode["size"]
-                generate_file(episode_path, episode_size)
-
-
-def verify_unprocessed_tv_shows(base_path, tv_shows_dictionary):
-    if not os.path.exists(base_path):
-        print(f"Failed! Base Path {base_path} does not exist")
-
-    tv_show_path = join(base_path, tv_shows_dictionary["name"])
-    actual_seasons = os.listdir(tv_show_path)
-    expected_seasons = tv_shows_dictionary["seasons"]
-    if len(actual_seasons) != len(expected_seasons):
-        print(f"Failed! Actual number of seasons {len(actual_seasons)} != expected {len(expected_seasons)}")
-
-    for i in range(len(actual_seasons)):
-        season_path = join(tv_show_path, actual_seasons[i])
-        if actual_seasons[i] != expected_seasons[i]["name"]:
-            print(f"Failed! Actual season name {actual_seasons[i]} not equal to expected season name {expected_seasons[i]['name']}")
-
-        actual_discs = os.listdir(season_path)
-        expected_discs = expected_seasons[i]["discs"]
-        if len(actual_discs) != len(expected_discs):
-            print(f"Failed! Actual number of discs {len(actual_discs)} != expected {len(expected_discs)}")
-
-        for j in range(len(actual_discs)):
-            disc_path = join(season_path, actual_discs[j])
-            if actual_discs[j] != expected_discs[j]["name"]:
-                print(f"Failed! Actual disc name {actual_discs[j]} not equal to expected disc name {expected_discs[j]['name']}")
-
-            actual_episodes = os.listdir(disc_path)
-            expected_episodes = expected_discs[j]["episodes"]
-            if len(actual_episodes) != len(expected_episodes):
-                print(f"Failed! Actual number of episodes {len(actual_episodes)} != expected {len(expected_episodes)}")
-            
-            for k in range(len(actual_episodes)):
-                if actual_episodes[k] != expected_episodes[k]["name"]:
-                    print(f"Failed! Acutal episode name {actual_episodes[k]} not equal to expected episode name {expected_episodes[k]['name']}")
-
-
-def verify_processed_tv_shows(base_path, tv_shows_dictionary):
-    if not os.path.exists(base_path):
-        print(f"Failed! Base Path {base_path} does not exist")
-
-    tv_show_path = join(base_path, tv_shows_dictionary["name"])
-    actual_seasons = os.listdir(tv_show_path)
-    expected_seasons = tv_shows_dictionary["seasons"]
-    if len(actual_seasons) != len(expected_seasons):
-        print(f"Failed! Actual number of seasons {len(actual_seasons)} != expected {len(expected_seasons)}")
-
-    for i in range(len(actual_seasons)):
-        season_path = join(tv_show_path, actual_seasons[i])
-        if actual_seasons[i] != expected_seasons[i]["name"]:
-            print(f"Failed! Actual season name {actual_seasons[i]} not equal to expected season name {expected_seasons[i]['name']}")
-
-        actual_episodes = os.listdir(season_path)
-        expected_episodes = expected_seasons[i]["episodes"]
-        if len(actual_episodes) != len(expected_episodes):
-            print(f"Failed! Actual number of episodes {len(actual_episodes)} != expected {len(expected_episodes)}")
-            
-        for j in range(len(actual_episodes)):
-            if actual_episodes[j] != expected_episodes[j]:
-                print(f"Failed! Actual episode name {actual_episodes[j]} not equal to expected episode name {expected_episodes[j]}")
-
-    
+   
 def generic_tv_shows_test():
     base_path = "tv_shows"
     number_of_tv_shows = 10
